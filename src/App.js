3b137dcf4fe833c8843e6e076523e4d7bac9e608
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2021-12-29 12:32:39 -0800

    Make text input clickable for users on mobile

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="fixed top-0 left-0 h-screen w-screen bg-black text-left text-primary">
         <span>$ </span>
-        <input autoFocus value={text} onChange={(e) => setText(e.target.value)} style={{width: `${text.length}ch`}} type="text" class="bg-black m-0 border-0 terminal-input"></input>
+        <input autoFocus value={text} onChange={(e) => setText(e.target.value)} style={{width: `${text.length == 0 ? 1 : text.length}ch`}} type="text" class="bg-black m-0 border-0 terminal-input"></input>
         <span class="animate-ping rounded-full text-primary">&#x0008;</span>
     </div>
   );
