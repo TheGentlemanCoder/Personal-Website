60c4510891d4a70330ff924eeaf9289f18d31a3b
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2021-12-29 23:37:54 -0800

    Add command history to terminal using the up and down arrows

--- src/App.js
+++ src/App.js
@@ -4,13 +4,40 @@ import './App.css';
 function App() {
     const [text, setText] = React.useState('');
     const [lines, setLines] = React.useState([]);
+    const [commandHistory, setCommandHistory] = React.useState([]);
+    const [commandHistoryPointer, setCommandHistoryPointer] = React.useState(0);
 
-    function checkNewline(e) {
+    function checkKeystroke(e) {
         if (e.key === 'Enter') {
             lines.push([text, true]); // add another line (true means to display caret)
             setLines(lines);
+
+            commandHistory.push(text); // save command
+            setCommandHistoryPointer(commandHistory.length); // point to empty prompt
+            setCommandHistory(commandHistory);
+            
             checkCommand(text); // see if the user has entered a valid command
             setText(''); // clear next line
+        }
+    }
+
+    function checkArrowKey(e) {
+        if (e.key === "ArrowUp") {
+            if (commandHistory.length > 0 && commandHistoryPointer > 0) {
+                // scroll to previous command
+                setCommandHistoryPointer(commandHistoryPointer - 1);
+                setText(commandHistory[commandHistoryPointer - 1]);
+            }
+        } else if (e.key === "ArrowDown") {
+            if (commandHistoryPointer == commandHistory.length - 1) {
+                // bring back a blank prompt
+                setCommandHistoryPointer(commandHistoryPointer + 1);
+                setText('');
+            } else if (commandHistoryPointer < commandHistory.length - 1) {
+                // scroll to following command
+                setCommandHistoryPointer(commandHistoryPointer + 1);
+                setText(commandHistory[commandHistoryPointer + 1]);
+            }
         }
     }
 
@@ -20,7 +47,8 @@ function App() {
                 window.location.href = "https://www.youtube.com/watch?v=a3Z7zEc7AXQ"; // hehe
                 break;
             case "clear":
-                setLines([]);
+                setLines([]); // clear the screen
+                setCommandHistory([]); // clear command history
                 break;
             case "help":
             case "?":
@@ -43,7 +71,10 @@ function App() {
         <div className="fixed top-0 left-0 h-screen w-screen bg-black text-left text-primary">
             <PreviousLines lines={lines}/>
             <span>$ </span>
-            <input autoFocus autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false" value={text} onChange={(e) => setText(e.target.value)} onKeyPress={(e) => checkNewline(e)} style={{width: `${text.length === 0 ? 1 : text.length}ch`}} type="text" className="bg-black m-0 border-0 terminal-input"></input>
+            <input autoFocus autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false" 
+                   value={text} onChange={(e) => setText(e.target.value)} onKeyPress={(e) => checkKeystroke(e)}
+                   onKeyDown={(e) => checkArrowKey(e)} style={{width: `${text.length === 0 ? 1 : text.length}ch`}}
+                   type="text" className="bg-black m-0 border-0 terminal-input"></input>
             <span className="animate-ping rounded-full text-primary">&#x0008;</span>
         </div>
     );
